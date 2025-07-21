8ed159d81738d722a4cdad593e260ddc2d41a092
Sammy Boy <samueltsutcliffe@gmail.com>
2025-07-21 02:10:05 +0100

    I added some middleware to allow for specific origin access, using cors

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 const port = 7000
+const cors = require('cors')
+
+
 const {client, dispose} = require('./DBacces')
 const {authorizeUser} = require('./DbOps')
 
+//enable cors for specific routes
+const allowedOrigins = [
+    'http://192.168.0.134:5173', // React dev server
+];
+
+app.use(cors({
+    origin: allowedOrigins, // Only allow these domains
+    methods: ['GET', 'POST'], // Allowed HTTP methods
+    credentials: true, // Enable cookies/auth headers (if needed)
+}));
 app.post('/register', (req, res) => {
     try {
         console.log("attempting to register");
