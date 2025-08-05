928e017080a0667ac01fe96473ab279154e42f11
Sammy Boy <samueltsutcliffe@gmail.com>
2025-08-05 13:58:40 +0100

    refactored the group endpoints to use one file for all of their operations

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const {
     patchUserData, getGroups, createGroup, deleteGroup
 } = require('./DbOps')
 
+const groupRoutes = require('./routes/groupRoutes');
+
 app.use(express.json());
 //enable cors for specific routes
 app.use(cors(
@@ -323,91 +325,93 @@ app.patch('/updateTask', async (req, res) => {
     }
 })
 
-app.get('/getGroups', async (req, res) => {
-    console.log("[Get Groups Endpoint] Starting group retrieval")
-    const connection = await getConnection();
-    console.log("[Get Groups Endpoint] Retrieving groups for user: " + req.query.username)
+// app.get('/getGroups', async (req, res) => {
+//     console.log("[Get Groups Endpoint] Starting group retrieval")
+//     const connection = await getConnection();
+//     console.log("[Get Groups Endpoint] Retrieving groups for user: " + req.query.username)
+//
+//     try {
+//         const result = await getGroups(connection, req.query.username)
+//         if (result.success) {
+//             res.status(200).send({
+//                 success: true,
+//                 groups: result.groups
+//             })
+//         } else {
+//             res.status(400).send({
+//                 success: false
+//             })
+//         }
+//     } catch (err) {
+//         console.error("[Get Groups Endpoint] Error retrieving groups for user: " + req.query.username  + " - " + err.message);
+//         res.status(500).send({
+//             success: false
+//         })
+//     }
+// })
 
-    try {
-        const result = await getGroups(connection, req.query.username)
-        if (result.success) {
-            res.status(200).send({
-                success: true,
-                groups: result.groups
-            })
-        } else {
-            res.status(400).send({
-                success: false
-            })
-        }
-    } catch (err) {
-        console.error("[Get Groups Endpoint] Error retrieving groups for user: " + req.query.username  + " - " + err.message);
-        res.status(500).send({
-            success: false
-        })
-    }
-})
+// app.post('/createGroup', async (req, res) => {
+//     console.log("[Create Group Endpoint] Starting group creation")
+//     const connection = await getConnection();
+//     console.log("[Create Group Endpoint] Creating group for user: " + req.body.username)
+//     try {
+//         console.log("[Create Group Endpoint] Calling database operation")
+//         const result = await createGroup(connection, req.body.username, req.body.groupName);
+//         if (result.success) {
+//             console.log("[Create Group Endpoint] Group created successfully:", result)
+//             return res.status(200).send({
+//                 success: true
+//             })
+//         } else {
+//             return res.status(400).send({
+//                 success: false
+//             })
+//         }
+//     } catch (err) {
+//         console.error("[Create Group Endpoint] Error creating group for user: " + req.body.username + " - " + err.message);
+//         res.status(500).send({
+//             success: false
+//         })
+//     }
+// })
 
-app.post('/createGroup', async (req, res) => {
-    console.log("[Create Group Endpoint] Starting group creation")
-    const connection = await getConnection();
-    console.log("[Create Group Endpoint] Creating group for user: " + req.body.username)
-    try {
-        console.log("[Create Group Endpoint] Calling database operation")
-        const result = await createGroup(connection, req.body.username, req.body.groupName);
-        if (result.success) {
-            console.log("[Create Group Endpoint] Group created successfully:", result)
-            return res.status(200).send({
-                success: true
-            })
-        } else {
-            return res.status(400).send({
-                success: false
-            })
-        }
-    } catch (err) {
-        console.error("[Create Group Endpoint] Error creating group for user: " + req.body.username + " - " + err.message);
-        res.status(500).send({
-            success: false
-        })
-    }
-})
+// app.delete('/deleteGroup', async (req, res) => {
+//     console.log("[Delete Group Endpoint] Starting group deletion")
+//     const connection = await getConnection();
+//     try {
+//         const { group, username } = req.body;
+//         console.log(`[Delete Group Endpoint] Attempting to delete group: ${group} for user: ${username}`);
+//
+//         if (!group || !username) {
+//             return res.status(400).send({
+//                 success: false
+//             });
+//         }
+//
+//         const result = await deleteGroup(connection, group, username);
+//
+//         if (result.success) {
+//             console.log("[Delete Group Endpoint] Group deleted successfully");
+//             return res.status(200).send({
+//                 success: true
+//             });
+//         } else {
+//             console.log("[Delete Group Endpoint] Could not delete group");
+//             return res.status(500).send({
+//                 success: false
+//             });
+//         }
+//     } catch (err) {
+//         console.error("[Delete Group Endpoint] Error: " + err.message);
+//         res.status(500).send({
+//             success: false
+//         });
+//     } finally {
+//         await releaseClient(connection);
+//     }
+// })
 
-app.delete('/deleteGroup', async (req, res) => {
-    console.log("[Delete Group Endpoint] Starting group deletion")
-    const connection = await getConnection();
-    try {
-        const { group, username } = req.body;
-        console.log(`[Delete Group Endpoint] Attempting to delete group: ${group} for user: ${username}`);
-        
-        if (!group || !username) {
-            return res.status(400).send({
-                success: false
-            });
-        }
-        
-        const result = await deleteGroup(connection, group, username);
-        
-        if (result.success) {
-            console.log("[Delete Group Endpoint] Group deleted successfully");
-            return res.status(200).send({
-                success: true
-            });
-        } else {
-            console.log("[Delete Group Endpoint] Could not delete group");
-            return res.status(500).send({
-                success: false
-            });
-        }
-    } catch (err) {
-        console.error("[Delete Group Endpoint] Error: " + err.message);
-        res.status(500).send({
-            success: false
-        });
-    } finally {
-        await releaseClient(connection);
-    }
-})
+app.use('/groups', groupRoutes)
 
 app.listen(port, () => {
     console.log(`[Server] API server listening on port ${port}`);
