0fea69750efae3cedcb898681a2801a617d6017c
Sammy Boy <samueltsutcliffe@gmail.com>
2025-08-27 03:08:58 +0100

    I have set up the main worker that will get the tasks from the database and check if the reminder date is today, if it is, then it will set the reminded value to true NOTE: need to add email sending thingy

--- emailWorker/emailWorker.js
+++ emailWorker/emailWorker.js
@@ -1,7 +1,56 @@
-//this will be a separate worker that will select from the task database every minute and check if those tasks
-//need to be notified.
+import {
+    getConnection,
+    releaseClient
+} from '../DBacces.js'
 
-//e.g. a user can set a date to notify them of a specific task.
+const client = await getConnection()
 
-//this worker will store all the tasks that are needing to be notified this day, and will send an email using another
-//module dependency at the time specified
+process.on('SIGINT', async () => {
+    console.log('Process interrupted. Closing DB pool...');
+    await releaseClient(client);
+    process.exit(0);
+});
+
+async function getReminders (connection) {
+
+    try {
+        // const query = `SELECT task.description, task.title, task.username, task.remind_date, task.id, users.email FROM task LEFT JOIN users ON task.username = users.username WHERE task.reminded = $1 AND task.remind_date IS NOT NULL`;
+        // const value = 'false';
+        // const result = await connection.query({
+        //     text: query,
+        //     values: [value],
+        // });
+
+        const result = await client.query(`SELECT task.description, task.title, task.username, task.remind_date, task.id, users.email FROM task LEFT JOIN users ON task.username = users.username WHERE task.reminded = 'false' AND task.remind_date IS NOT NULL`)
+
+        const today = new Date();
+
+        const month = today.getMonth() + 1
+        const date = today.getDate()
+        const year = today.getFullYear()
+        const onlyDate = `${date}/${month}/${year}`
+
+
+        if (result.rows.length > 0) {
+            console.info(`[Worker] - Reminders ${result.rows.length} rows`);
+            for (let row of result.rows) {
+                if (row.remind_date === onlyDate) {
+                    const task_id = row.id
+                    connection.query(`UPDATE task SET reminded = TRUE WHERE id = $1`, [task_id])
+                    console.info(`[Worker] - Reminder sent for task: ${task_id}`)
+                }
+            }
+        } else {
+            console.error("[Worker] - There are no tasks to be reminded of for date: " + onlyDate);
+        }
+    } catch (e) {
+        // await releaseClient(connection);
+        console.error("[Worker] - Error getting reminders: \n" + e.message)
+    }
+}
+
+await getReminders(client)
+
+setInterval(async ()  => getReminders(client), 60 * 1000)
+
+console.log('[Worker] Task reminder worker started.');
