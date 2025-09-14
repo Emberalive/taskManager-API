e081cfbad38765c2198c3e5de49737c2ae075f2b
Sammy Boy <samueltsutcliffe@gmail.com>
2025-09-14 01:25:32 +0100

    I have finished the email worker, i have been messing around with fixing date formats so that i can check the database date with the current date.

--- emailWorker/emailWorker.js
+++ emailWorker/emailWorker.js
@@ -1,22 +1,36 @@
-import {
+const {
     getConnection,
     releaseClient
-} from '../DBacces.js'
+} = require('../DBacces.js')
 
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv';
-import path from 'path';
+const {format, parseISO} = require("date-fns")
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv');
+const path = require('path');
+const {getLogger} = require("nodemailer/lib/shared");
 dotenv.config({ path: path.resolve('../.env') });
 
 dotenv.config();
 
-const client = await getConnection()
+let client;
 
-process.on('SIGINT', async () => {
-    console.log('Process interrupted. Closing DB pool...');
-    await releaseClient(client);
-    process.exit(0);
-});
+async function startWorker() {
+    try{
+        client = await getConnection();
+
+        process.on('SIGINT', async () => {
+            console.log('Process interrupted. Closing DB pool...');
+            await releaseClient(client);
+            process.exit(0);
+        });
+
+        await getReminders(client);
+
+        setInterval(async () => getReminders(client), 60 * 1000);
+    } catch(e) {
+        console.error(`Error getting connection to the database: ${e.message}`);
+    }
+}
 
 async function getReminders (connection) {
 
@@ -24,18 +38,21 @@ async function getReminders (connection) {
 
         const result = await client.query(`SELECT task.description, task.title, task.username, task.remind_date, task.id, users.email FROM task LEFT JOIN users ON task.username = users.username WHERE task.reminded = 'false' AND task.remind_date IS NOT NULL`)
 
-        const today = new Date();
+        const today = format(new Date(), "yyyy-MM-dd");
 
-        const month = today.getMonth() + 1
-        const date = today.getDate()
-        const year = today.getFullYear()
-        const onlyDate = `${year}-${month}-${date}`
-
+        console.log('[Worker] Task reminder worker started.');
 
         if (result.rows.length > 0) {
             console.info(`[Worker] - Reminders ${result.rows.length} rows`);
             for (let row of result.rows) {
-                if (row.remind_date === onlyDate) {
+                //parses date format in database to Date.now() format, so i can check it
+                const dbDate = parseISO(row.remind_date);
+                const dbDay = format(dbDate, "yyyy-MM-dd");
+
+                console.log(`Today's date: ${format(new Date(), "yyyy-MM-dd")}`);
+                console.log(`[Worker] - Checking date for task ${row.id}, with date: ${dbDay}`);
+
+                if (dbDay === today) {
                     const { id: task_id, title: task_title, description: task_description } = row;
 
                     if (task_id && task_description && task_title) {
@@ -56,22 +73,25 @@ async function getReminders (connection) {
 
                         connection.query(`UPDATE task SET reminded = TRUE WHERE id = $1`, [task_id])
 
-                        console.info(`[Worker] - Reminder sent for task: ${task_id} \n Email: ${email.messageId} \n`)
+                        console.info(`[Worker] - Reminder sent for task: ${task_id} \n      Email: ${email.messageId} \n`)
+                        console.log("")
                     } else {
                         console.error(`[Worker] - Reminder unsent for task (incorrect parameters received): ${task_id}`)
                     }
+                } else {
+                    console.log(`reminder for task: ${id} was not today`)
                 }
+                console.log("Next reminder to be checked")
             }
         } else {
-            console.error("[Worker] - There are no tasks to be reminded of for date: " + onlyDate);
+            console.error(
+                "[Worker] - There are no tasks to be reminded of for date: " +
+                today // local YYYY-MM-DD
+            );
         }
     } catch (e) {
         console.error("[Worker] - Error getting reminders: \n" + e.message)
     }
 }
 
-await getReminders(client)
-
-setInterval(async ()  => getReminders(client), 60 * 1000)
-
-console.log('[Worker] Task reminder worker started.');
+startWorker().catch(error => console.error(error))
